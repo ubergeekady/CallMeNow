bfc80c635d9384aa3bfae1dcd328d88a2dcf95e7
adityasingh <aditya@impulsemedia.co.in>
2017-12-08 20:31:20 +0530

    DB Changes

--- callmenow/settings.py
+++ callmenow/settings.py
@@ -85,14 +85,14 @@ WSGI_APPLICATION = 'callmenow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
+DATABASES1 = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-DATABASES1 = {
+DATABASES = {
   'default': {
   'ENGINE': 'django.db.backends.postgresql_psycopg2',
   'NAME': 'callmenow',
